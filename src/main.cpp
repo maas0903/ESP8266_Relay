251e46fd9dc228a0ccda0c7fbf31bef0fed3717a
maas0903 <marius.tennapel@hotmail.com>
2019-07-31 17:28:02 +0200

    Sorted out CORS

--- src/main.cpp
+++ src/main.cpp
@@ -108,13 +108,14 @@ void get_temps()
     else
     {
         jsonObj["UtcTime"] = currentTime;
-        jsonObj["Epoch time"] = epochTime;
+        jsonObj["EpochTime"] = epochTime;
         jsonObj["ThermometerId"] = deviceAddress;
         jsonObj["Temperature"] = temperature;
         jsonObj["Hostname"] = hostname;
         jsonObj["IpAddress"] = ipAddress;
         jsonObj["Gpio"] = gpio;
         jsonObj.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
+        http_rest_server.sendHeader("Access-Control-Allow-Origin", "*");
         http_rest_server.send(200, "application/json", JSONmessageBuffer);
     }
 }
