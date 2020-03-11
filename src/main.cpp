d0e7061ee2ab50d4ce19408a4176fcab718522d7
Marius <maas0903@gmail.com>
2020-03-11 23:09:04 +0100

    added 2 sensors

--- src/main.cpp
+++ src/main.cpp
@@ -25,17 +25,34 @@ NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
+float tempSensor0;
 float tempSensor1;
-DeviceAddress Thermometer;
+DeviceAddress Thermometer0;
+DeviceAddress Thermometer1;
+uint8_t sensor0[8];
 uint8_t sensor1[8];
 
-String deviceAddress = "";
+String deviceAddress0 = "";
+String deviceAddress1 = "";
 byte gpio = 2;
-String temperature = "-127";
+String strTemperature0 = "-127";
+String strTemperature1 = "-127";
 String ipAddress = "127.0.0.0";
 String hostname = "";
 
 ESP8266WebServer http_rest_server(HTTP_REST_PORT);
+
+void BlinkNTimes(int pin, int blinks, unsigned long millies)
+{
+    digitalWrite(pin, LOW);
+    for (int i = 0; i < blinks; i++)
+    {
+        digitalWrite(pin, HIGH);
+        delay(millies);
+        digitalWrite(pin, LOW);
+        delay(millies);
+    }
+}
 
 int init_wifi()
 {
@@ -51,11 +68,13 @@ int init_wifi()
         retries++;
         delay(WIFI_RETRY_DELAY);
         Serial.print("#");
+        BlinkNTimes(LED_0, 1, 200);
     }
     Serial.println();
     hostname = WiFi.hostname();
-    Serial.println("hostname = "+hostname);
+    Serial.println("hostname = " + hostname);
     return WiFi.status();
+    BlinkNTimes(LED_0, 3, 500);
 }
 
 String GetAddressToString(DeviceAddress deviceAddress)
@@ -78,6 +97,8 @@ void get_temps()
     timeClient.update();
     unsigned long epochTime = timeClient.getEpochTime();
     char buff[32];
+    int deviceCount = sensors.getDeviceCount();
+
     sprintf(buff, "%02d-%02d-%02d %02d:%02d:%02d",
             year(epochTime),
             month(epochTime),
@@ -87,33 +108,40 @@ void get_temps()
             second(epochTime));
     String currentTime = buff;
 
-    Serial.println(currentTime);
-
     sensors.requestTemperatures();
+    tempSensor0 = sensors.getTempC(sensor0);
     tempSensor1 = sensors.getTempC(sensor1);
 
-    deviceAddress = GetAddressToString(Thermometer);
-    temperature = tempSensor1;
+    deviceAddress0 = GetAddressToString(Thermometer0);
+    deviceAddress1 = GetAddressToString(Thermometer1);
+    strTemperature0 = tempSensor0;
+    strTemperature1 = tempSensor1;
     ipAddress = WiFi.localIP().toString();
 
     StaticJsonBuffer<400> jsonBuffer;
     JsonObject &jsonObj = jsonBuffer.createObject();
     char JSONmessageBuffer[400];
 
-    if (deviceAddress.equals(""))
+    if (deviceAddress0.equals("") && deviceAddress1.equals(""))
     {
         Serial.print("No Content");
         http_rest_server.send(204);
     }
     else
     {
-        jsonObj["UtcTime"] = currentTime;
-        jsonObj["EpochTime"] = epochTime;
-        jsonObj["ThermometerId"] = deviceAddress;
-        jsonObj["Temperature"] = temperature;
-        jsonObj["Hostname"] = hostname;
-        jsonObj["IpAddress"] = ipAddress;
-        jsonObj["Gpio"] = gpio;
+        //for (int i = 0; i < deviceCount; i++)
+        {
+            jsonObj["UtcTime"] = currentTime;
+            jsonObj["EpochTime"] = epochTime;
+            jsonObj["DeviceCount"] = deviceCount;
+            jsonObj["ThermometerId0"] = deviceAddress0;
+            jsonObj["ThermometerId1"] = deviceAddress1;
+            jsonObj["Temperature0"] = strTemperature0;
+            jsonObj["Temperature1"] = strTemperature1;
+            jsonObj["Hostname"] = hostname;
+            jsonObj["IpAddress"] = ipAddress;
+            jsonObj["Gpio"] = gpio;
+        }
         jsonObj.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
         http_rest_server.sendHeader("Access-Control-Allow-Origin", "*");
         http_rest_server.send(200, "application/json", JSONmessageBuffer);
@@ -132,14 +160,25 @@ void config_rest_server_routing()
 void setup(void)
 {
     Serial.begin(115200);
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(LED_0, OUTPUT);
 
     sensors.begin();
-    if (sensors.getAddress(Thermometer, 0))
+    if (sensors.getAddress(Thermometer0, 0))
     {
         //Take the first sensor as the measuring sensor
         for (uint8_t i = 0; i < 8; i++)
         {
-            sensor1[i] = Thermometer[i];
+            sensor0[i] = Thermometer0[i];
+        }
+    }
+
+    if (sensors.getAddress(Thermometer1, 1))
+    {
+        //Take the first sensor as the measuring sensor
+        for (uint8_t i = 0; i < 8; i++)
+        {
+            sensor1[i] = Thermometer1[i];
         }
     }
 
